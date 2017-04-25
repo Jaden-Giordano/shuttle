3cc34c5665a2d990b502e8dea64284b88c2d2061
Jaden-Giordano <thejadenjack@gmail.com>
2017-04-25 16:14:05 -0600

    change game to a var

--- src/public/main.js
+++ src/public/main.js
@@ -2,7 +2,7 @@ window.PIXI = require('phaser/build/custom/pixi');
 window.p2 = require('phaser/build/custom/p2');
 window.Phaser = require('phaser/build/custom/phaser-split');
 
-let game = new Phaser.Game(800, 800, Phaser.AUTO, '', {
+var game = new Phaser.Game(800, 800, Phaser.AUTO, '', {
   preload: () => {
 
   },
