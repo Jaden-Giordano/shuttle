3fb0eefee2b13743cc4c38ddcb0c4d4bfd302d36
Jaden-Giordano <thejadenjack@gmail.com>
2017-04-25 19:23:59 -0600

    Created game.js
    should handle base game loops, states

---
+++ src/public/game.js
@@ -0,0 +1,17 @@
+window.PIXI = require('phaser/build/custom/pixi');
+window.p2 = require('phaser/build/custom/p2');
+window.Phaser = require('phaser/build/custom/phaser-split');
+
+var game = new Phaser.Game(800, 800, Phaser.AUTO, '', {
+  preload: () => {
+
+  },
+  create: () => {
+
+  },
+  update: () => {
+
+  }
+});
+
+export default game;
--- src/public/main.js
+++ src/public/main.js
@@ -1,15 +1,1 @@
-window.PIXI = require('phaser/build/custom/pixi');
-window.p2 = require('phaser/build/custom/p2');
-window.Phaser = require('phaser/build/custom/phaser-split');
-
-var game = new Phaser.Game(800, 800, Phaser.AUTO, '', {
-  preload: () => {
-
-  },
-  create: () => {
-
-  },
-  update: () => {
-
-  }
-});
+import Game from './game';
