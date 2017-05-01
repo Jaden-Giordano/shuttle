147bc5f93948bb00f190e7489ab8bd9dd8750350
Jaden-Giordano <thejadenjack@gmail.com>
2017-05-01 08:04:12 -0600

    States

--- src/public/game.js
+++ src/public/game.js
@@ -1,10 +1,16 @@
-import Main from './states/main.js';
+import Boot from './states/boot';
+import Preload from './states/preload';
+import Main from './states/main';
 
 class Game extends Phaser.Game {
   constructor() {
     super(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO);
 
+    this.state.add('Boot', Boot, false);
+    this.state.add('Preload', Preload, false);
     this.state.add('Main', Main, false);
+
+    this.state.start('Boot');
   }
 }
 
