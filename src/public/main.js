9dce82f9a482050a1bbd8b5ac20badde871ea3ab
Greymouth <thejadenjack@gmail.com>
2017-04-30 22:13:15 -0600

    Fixed Phaser

--- src/public/main.js
+++ src/public/main.js
@@ -1,7 +1,3 @@
-window.PIXI = require('phaser/build/custom/pixi');
-window.p2 = require('phaser/build/custom/p2');
-window.Phaser = require('phaser/build/custom/phaser-split');
-
 import Game from './game';
 
 new Game();
--- src/public/states/main.js
+++ src/public/states/main.js
@@ -1,3 +1,7 @@
+window.PIXI = require('phaser/build/custom/pixi');
+window.p2 = require('phaser/build/custom/p2');
+window.Phaser = require('phaser/build/custom/phaser-split');
+
 class Main extends Phaser.State {
   create() {
 
