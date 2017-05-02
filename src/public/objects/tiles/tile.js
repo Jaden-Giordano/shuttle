92bf27d3ddc8fbe6e00db4d3ff2d73991ff43267
Greymouth <thejadenjack@gmail.com>
2017-05-02 00:03:49 -0600

    Drawing tiles

--- src/public/objects/tiles/tile.js
+++ src/public/objects/tiles/tile.js
@@ -1,6 +1,8 @@
 class Tile {
-  constructor(game, id) {
+  constructor(game, id, x, y, frame) {
     this.id = id;
+
+    this.sprite = game.add.sprite(x, y, frame, id);
   }
 }
 
--- src/public/states/main.js
+++ src/public/states/main.js
@@ -1,6 +1,8 @@
+import Tile from '../objects/tiles/tile';
+
 class Main extends Phaser.State {
   create() {
-
+    this.testTile = new Tile(this.game, 0, 0, 0, 'FloorTiles');
   }
 
   update() {
