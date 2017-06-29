5bdb27b98a63fd413f0f0b0b49f8b14c57b04101
icyhate <icyhate@gmail.com>
2017-06-29 16:59:46 -0500

    added to assetloader

--- src/public/asset-loader.js
+++ src/public/asset-loader.js
@@ -9,6 +9,10 @@ function getDirectories(srcpath) {
   return fs.readdirSync(srcpath).filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory());
 }
 
+function getFiles(srcpath) {
+  return fs.readdirSync(srcpath).filter(file => fs.lstatSync(path.join(srcpath, file)).isFile());
+}
+
 function loadMods() {
   let dirs = [];
   dirs = getDirectories(assetsFolder);
@@ -19,7 +23,7 @@ function loadMods() {
 
   var mods = walkFoldersOfPath(assetsFolder);
 
-  console.log(mods);
+  console.log(JSON.stringify(mods, null, 4));
 
   for (var i = 0; i < dirs.length; i++) {
     let assetdirs = getDirectories(path.join(assetsFolder, dirs[i] + '/'));
@@ -31,11 +35,33 @@ function loadMods() {
 }
 
 function walkFoldersOfPath(folder){
+  let splitpaths = folder.replace(assetsFolder, '').replace(/\\/g, '/').split('/');
+  let assetfilter = '';
+  if(splitpaths.length > 2){
+    switch(splitpaths[1]){
+      case 'maps':
+          assetfilter = splitpaths[1];
+        break;
+      case 'objects':
+          assetfilter = splitpaths[1];
+        break;
+      case 'sprites':
+          assetfilter = splitpaths[1];
+        break;
+      case 'sounds':
+          assetfilter = splitpaths[1];
+        break;
+    }
+  }
   let assetObject = {};
   let dirs = getDirectories(folder);
+  let files = getFiles(folder);
   for(let i = 0; i < dirs.length; i++){
     assetObject[dirs[i]] = Object();
     assetObject[dirs[i]] = walkFoldersOfPath(path.join(folder, dirs[i] + '/'));
+  }
+  for(let i = 0; i < files.length; i++){
+    assetObject[files[i]] = folder.replace(assetsFolder, '').replace(/\//g, '.') + files[i];
   }
   return assetObject;
 }
